5e7b19a0431e8b65a190573be3b216837f7f0249
Yugam Verma <yugamverma003@gmail.com>
2024-07-24 11:28:40 +0530

    Random commit...

--- src/components/profile/Dashboard.tsx
+++ src/components/profile/Dashboard.tsx
@@ -13,6 +13,7 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 import {useRecoilValue } from "recoil";
 import { userDetails } from "@/recoil/atoms/userState";
+import Loading from "../loading/Loading";
 
 function SidebarDemo() {
   const user = useRecoilValue(userDetails);
@@ -126,7 +127,8 @@ const Dashboard = () => {
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         
-        
+        <Loading />
+
       </div>
     </div>
   );
